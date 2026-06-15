27500eca7f9784c2acbdf3f3f2bdb0ca5411ecda
eduardoborgs <eduardo.b.santos6@ba.estudante.senai.br>
2026-06-15 10:08:27 -0300

    fix: adiciona disfarce de client android para burlar youtube

--- backend/services/downloader.py
+++ backend/services/downloader.py
@@ -20,8 +20,15 @@ class MediaDownloader:
             "quiet": True,
             "no_warnings": True,
             "no_color": True,
+            # Configurações Anti-Bloqueio do YouTube
+            "extractor_args": {
+                "youtube": ["client=android,mweb"]
+            },
+            "http_headers": {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+            }
         }
-
+    
     async def get_info(self, url: str) -> MediaInfo:
         if not is_safe_url(url):
             raise ValueError("URL inválida ou não permitida.")
